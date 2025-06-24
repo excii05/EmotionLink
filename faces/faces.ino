2775c0544d96974ed78bcd214d1b9b030dc00cde
Tim <t.fuhrmann@krentruperhagen.de>
2025-06-25 00:01:36 +0200

    Updated faces.ino and images

--- faces/faces.ino
+++ faces/faces.ino
@@ -17,6 +17,9 @@ Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 void setup() {
   Serial.begin(115200);
 
+  // Set I2C pins explicitly!
+  Wire.begin(3, 1); // SDA = GPIO3 (TX), SCL = GPIO1 (RX)
+
   // Initialize the display
   if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
     Serial.println("SSD1306 allocation failed");
@@ -28,16 +31,16 @@ void setup() {
 }
 
 void loop() {
-  showEmotion(happy_face_eyebrows_bits, "Happy");
+  showEmotion(happy_face_bits, "Happy");
   delay(2000);
 
-  showEmotion(neutral_face_eyebrows_bits, "Neutral");
+  showEmotion(neutral_face_bits, "Neutral");
   delay(2000);
 
-  showEmotion(sad_face_eyebrows_bits, "Sad");
+  showEmotion(sad_face_bits, "Sad");
   delay(2000);
 
-  showEmotion(angry_face_eyebrows_bits, "Angry");
+  showEmotion(angry_face_bits, "Angry");
   delay(2000);
 }
 
